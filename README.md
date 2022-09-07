3f27cdd951125e801473c352e1daecebd940104d
James McCormac <jmccormac001@gmail.com>
2022-09-07 11:16:34 +0100

    initial commit of data and crossmatch

---
+++ README.md
@@ -0,0 +1,6 @@
+# Crossmatch Open Clusters to TIC8
+
+Take a list of OC cluster members and fetch more magnitudes
+from TIC8 for each.
+
+
---
+++ oc_tic8_crossmatch.py
@@ -0,0 +1,68 @@
+"""
+Take the file from:
+    https://ui.adsabs.harvard.edu/abs/2021ApJ...923..129J/abstract
+
+and crossmatched with TIC8 to get magnitudes for all cluster members
+
+Output the new crossmatched table as csv file
+"""
+from contextlib import contextmanager
+import pymysql
+
+def query_object(gaia_id):
+    """
+    Get magnitudes for given object
+    """
+    qry = """
+        SELECT
+        e_Bmag,Vmag,e_Vmag,umag,e_umag,
+        gmag,e_gmag,rmag,e_rmag,imag,
+        e_imag,zmag,e_zmag,Jmag,e_Jmag,
+        Hmag,e_Hmag,Kmag,e_Kmag
+        FROM tic8
+        WHERE gaia_id=%s
+        """.format(gaia_id)
+
+    with open_db() as cur:
+        cur.query(qry)
+        res = cur.fetchone()
+    return res
+
+@contextmanager
+def open_db(host='ngtdb', db='catalogues'):
+    """
+    Connect to database
+    """
+    with pymysql.connection() as conn:
+        with conn.cursor() as cur:
+            yield cur
+
+
+
+if __name__ == "__main__":
+    # load up the list of cluster members from the
+    # Jaehnig K., Bird J., Holley-Bockelmann K. 2021 paper
+    members_path = "open_cluster_membership_apjac1d51t2_mrt.txt"
+
+    # load the catalog file
+    catalog = pd.read_fwf(members_path,
+                          colspecs=[(0, 17),
+                                    (17, 37),
+                                    (37, 62),
+                                    (62, 83),
+                                    (83, 108),
+                                    (108, 129),
+                                    (129, 153),
+                                    (153, 174),
+                                    (174, 198),
+                                    (198, 219),
+                                    (219, 243),
+                                    (243, 264),
+                                    (264, 275),
+                                    (275, 290),
+                                    (290, 294)],
+                          skiprows=30, header=0)
+
+    test_id = "458720619099467520"
+
+    results = query_object(test_id)
